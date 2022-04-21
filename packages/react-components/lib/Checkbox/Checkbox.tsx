2e9ea120d24e3d124b0117756db9c4da75dacb30
Darin Cassler <darin@cassler.net>
2022-04-21 12:42:45 -0500

    another pass at cleanups

--- packages/react-components/lib/Checkbox/Checkbox.tsx
+++ packages/react-components/lib/Checkbox/Checkbox.tsx
@@ -11,29 +11,41 @@ export interface IProps extends HTMLProps<HTMLInputElement> {
   label: string
 }
 
-export const Checkbox:React.FC<IProps>  = ({ label = '', checked = false, ...props }) => {
+export const Checkbox:React.FC<IProps>  = ({
+  label = '',
+  checked = false,
+  className = '',
+  onChange = () => {},
+  onToggle = () => {},
+  onClick = () => {},
+  ...props
+}) => {
   const cbRef = React.useRef<HTMLInputElement>(null);
-
   const [isChecked, setChecked] = React.useState(checked);
-  const { onChange, onToggle, className } = props;
-
   const baseClasses = 'flex gap-2 items-center cursor-pointer';
   const clsx = [baseClasses, className].join(' ');
 
   function handleClick(e:React.BaseSyntheticEvent) {
+    onClick(e as React.MouseEvent<any>);
     e.currentTarget = cbRef.current;
     e.currentTarget.checked = !isChecked;
-    if (onChange) onChange(e as React.FormEvent<HTMLInputElement>);
-    if (onToggle) onToggle([label, !isChecked]);
+    onChange(e as React.FormEvent<HTMLInputElement>);
+    onToggle([label, !isChecked]);
     setChecked(!isChecked);
   }
 
   return (
-    <div aria-name={label} className={clsx} onClick={handleClick}>
-      <input name={label} ref={cbRef} type='checkbox' checked={isChecked} onChange={() => {}} />
+    <div className={clsx} onClick={handleClick} {...props}>
+      <input
+        name={label}
+        ref={cbRef}
+        type='checkbox'
+        checked={isChecked}
+        onChange={handleClick} />
       <span className='prism-label'>{label}</span>
     </div>
   );
 };
+
 
 export default Checkbox;
